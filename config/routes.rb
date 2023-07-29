abd54395391b587862493824e70cc1796461f8f3
Priscila Finkler <prifinkler@gmail.com>
2023-07-29 12:48:04 +0100

    added task model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # verb "path", to: "controller#action"
+  get 'tasks', to: 'tasks#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_29_111117) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_114218) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "false"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
